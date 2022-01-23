9e1e4f7157820c7390241d3b4c558ad8e4915e4a
YunSoobin <soobin5296@gmail.com>
2022-01-23 23:16:15 +0900

    게임 개발

---
+++ Algorithm02_Implementation/Q02_게임개발.py
@@ -0,0 +1,13 @@
+'''
+- 시뮬레이션 문제 구현에 어려움이 있어 해설을 참조하여 학습한 후 다시 풀어 보았다.
+- "일반적으로 direction을 설정해서 이동하는 문제 유형에서는 dx, dy라는 별도의 리스트를 만들어 방향을 정하는 것이 효과적이다."
+'''
+
+
+import time
+start_time = time.time()
+
+
+
+end_time = time.time()
+print("time : ", end_time - start_time)
