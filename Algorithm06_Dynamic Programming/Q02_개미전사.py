a0ca7eb710a0324e10f068069c927ffd39baff8b
YunSoobin <soobin5296@gmail.com>
2022-02-09 22:59:35 +0900

    개미 전사

---
+++ Algorithm06_Dynamic Programming/Q02_개미전사.py
@@ -0,0 +1,17 @@
+'''
+- 문제 해결을 위한 아이디어를 떠올리는 데에 어려움이 있어
+    해설을 참조하여 학습한 후 다시 풀어 보았다.
+
+'''
+
+
+import time
+start_time = time.time()
+
+N = int(input())
+
+
+
+
+end_time = time.time()
+print("time  : ", end_time - start_time)
